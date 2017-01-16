faee8baa8a0dd259051a27a85a78e2d9d5926dd2
root <root@localhost.localdomain>
2017-01-16 11:02:14 +0200

    fr_memmove_update

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,27 +14,49 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	char		*tmp;
-	char		*sdest;
-	const char	*ssrc;
+		/*size_t		i;
+		  char		*tmp;
+		  char		*sdest;
+		  const char	*ssrc;
 
-	i = 0;
-	ssrc = (const char*)src;
-	sdest = (char*)dest;
-	if (!(tmp = (char *)malloc(sizeof(*tmp) * n + 1)))
-		return (NULL);
-	while (i < n)
-	{
-		tmp[i] = ssrc[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		sdest[i] = (char)tmp[i];
-		i++;
-	}
-	free(tmp);
-	return (dest);
+		  i = 0;
+		  ssrc = (const char*)src;
+		  sdest = (char*)dest;
+		  if (!(tmp = (char *)malloc(sizeof(*tmp) * n + 1)))
+		  return (NULL);
+		  while (i < n)
+		  {
+		  tmp[i] = ssrc[i];
+		  i++;
+		  }
+		  i = 0;
+		  while (i < n)
+		  {
+		  sdest[i] = (char)tmp[i];
+		  i++;
+		  }
+		  free(tmp);
+		  return (dest);*/
+
+		size_t		i;
+
+		i = 0;
+		if (src < dest)
+		{
+				i = n;
+				while (i > 0)
+				{
+						i--;
+						((char *)dest)[i] = ((char *)src)[i];
+				}
+		}
+		else
+		{
+				while (i < n)
+				{
+						((char *)dest)[i] = ((char *)src)[i];
+						i++;
+				}
+		}
+		return (dest);
 }
