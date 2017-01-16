4fcf0e1effd44be45e3877124ae09ad9e5ae0dd1
root <root@localhost.localdomain>
2017-01-16 10:55:38 +0200

    libft

---
+++ ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkoekemo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/22 21:44:31 by lkoekemo          #+#    #+#             */
+/*   Updated: 2016/11/01 11:23:46 by lkoekemo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	char		*tmp;
+	char		*sdest;
+	const char	*ssrc;
+
+	i = 0;
+	ssrc = (const char*)src;
+	sdest = (char*)dest;
+	if (!(tmp = (char *)malloc(sizeof(*tmp) * n + 1)))
+		return (NULL);
+	while (i < n)
+	{
+		tmp[i] = ssrc[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		sdest[i] = (char)tmp[i];
+		i++;
+	}
+	free(tmp);
+	return (dest);
+}
